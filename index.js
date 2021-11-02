56853851f6dcd8ef62d452bbce1cb10ee0b857f1
Naima TOURAGHE <80955884+naimatouraghe@users.noreply.github.com>
2021-11-02 07:47:11 +0100

    Calculator commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const button = document.querySelectorAll('.btn');
+
+const result = document.getElementById('result');
+
+button.forEach((button) => {
+    button.addEventListener('click', (e) =>{
+        result.textContent += e.target.id; 
+    });
+});
+
+
+equal.addEventListener('click', () =>{
+    result.textContent = eval(result.textContent);
+})
+
+
+clear.addEventListener('click', () =>{
+    result.textContent = "";
+});
